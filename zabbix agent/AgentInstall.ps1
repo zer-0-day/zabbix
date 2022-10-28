ee5249bddcdb355c0f3d18f20f4c98bf4ed4a9e3
Дмитрий А. Чумбаев <dmitrij.chumbaev@office.partner-its.ru>
2022-10-28 16:52:39 +0300

    change install script

--- zabbix agent/AgentInstall.ps1
+++ zabbix agent/AgentInstall.ps1
@@ -1,22 +1,25 @@
 [Console]::OutputEncoding = [System.Text.Encoding]::GetEncoding("utf-8")
             
-                #Подготовка конфига агента
-
-
+    #Подготовка конфига агента
     #Получить имя хоста
 $ClienID = Get-ChildItem -Path "C:\Program Files\zabbix\" *.key | Select-Object BaseName
 $ClienID.BaseName
-
     #замена partnerid на имя из файла .key
-    
 $FILE = Get-Content -Path "C:\Program Files\zabbix\conf\zabbix_agentd2.conf"
 $NEWFILE = $FILE -replace 'partnerid', $ClienID.BaseName
 $NEWFILE | Set-Content -Path "C:\Program Files\zabbix\conf\zabbix_agentd2.conf"
-
-
-#cmd.exe
+    #Установка агента
+cmd.exe
 "C:\Program Files\zabbix\bin\zabbix_agent2.exe" --c "C:\Program Files\zabbix\conf\zabbix_agentd2.conf" --install
-#exit
+exit
 "Создайте новый узел сети на сервере Zabbix, затем продолжите работу установщика"
 
+Start-Service zab*
+Get-Service zab*
+timeout.exe /T 20
+Stop-Service zab*
+Start-Service zab*
+Get-Service zab*
+Get-Content -Path "C:\Program Files\zabbix\zabbix_agentd.log"
+Pause
 
