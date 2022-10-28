a87ced4deffe1ec3ad9e9bdd10f796c0d0a248a5
Дмитрий А. Чумбаев <dmitrij.chumbaev@office.partner-its.ru>
2022-10-28 16:40:26 +0300

    change install script

--- zabbix agent/AgentInstall.ps1
+++ zabbix agent/AgentInstall.ps1
@@ -1,13 +1,22 @@
 [Console]::OutputEncoding = [System.Text.Encoding]::GetEncoding("utf-8")
+            
+                #Подготовка конфига агента
+
+
+    #Получить имя хоста
+$ClienID = Get-ChildItem -Path "C:\Program Files\zabbix\" *.key | Select-Object BaseName
+$ClienID.BaseName
+
+    #замена partnerid на имя из файла .key
+    
+$FILE = Get-Content -Path "C:\Program Files\zabbix\conf\zabbix_agentd2.conf"
+$NEWFILE = $FILE -replace 'partnerid', $ClienID.BaseName
+$NEWFILE | Set-Content -Path "C:\Program Files\zabbix\conf\zabbix_agentd2.conf"
+
+
 #cmd.exe
 "C:\Program Files\zabbix\bin\zabbix_agent2.exe" --c "C:\Program Files\zabbix\conf\zabbix_agentd2.conf" --install
 #exit
 "Создайте новый узел сети на сервере Zabbix, затем продолжите работу установщика"
-#Получить имя хоста
-$ClienID = Get-ChildItem -Path "C:\Program Files\zabbix\" *.key | Select-Object BaseName
-$ClienID.BaseName
-#замена partnerid на имя из файла .key
-$FILE = Get-Content -Path "C:\Program Files\zabbix\conf\zabbix_agentd2.conf"
-$NEWFILE = $FILE -replace 'partnerid', $ClienID.BaseName
-$NEWFILE | Set-Content -Path "C:\Program Files\zabbix\conf\zabbix_agentd2.conf"
+
 
